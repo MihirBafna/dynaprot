2063ce5d5fb0a799505f90b02b4b5b33b1aa0e01
MihirBafna <mihir.bafna@gmail.com>
2024-10-30 22:45:02 -0400

    added data preprocessing, loading, collator, transforms, etc

---
+++ DynaProt/data/transforms.py
@@ -0,0 +1,28 @@
+
+import torch
+import itertools
+
+
+def make_fixed_size(protein, shape_schema, num_residues=0):
+    pad_size_map = {"NUM_RES":num_residues}
+
+    for k, v in protein.items():
+        # Don't transfer this to the accelerator.
+        if k == "extra_cluster_assignment":
+            continue
+        shape = list(v.shape)
+        schema = shape_schema[k]
+        msg = "Rank mismatch between shape and shape schema for"
+        assert len(shape) == len(schema), f"{msg} {k}: {shape} vs {schema}"
+        pad_size = [
+            pad_size_map.get(s2, None) or s1 for (s1, s2) in zip(shape, schema)
+        ]
+
+        padding = [(0, p - v.shape[i]) for i, p in enumerate(pad_size)]
+        padding.reverse()
+        padding = list(itertools.chain(*padding))
+        if padding:
+            protein[k] = torch.nn.functional.pad(v, padding)
+            protein[k] = torch.reshape(protein[k], pad_size)
+    
+    return protein
