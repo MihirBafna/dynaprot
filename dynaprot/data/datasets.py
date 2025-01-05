6677731949362b71b12fcc83cc6f28f5d3464503
MihirBafna <mihir.bafna@gmail.com>
2025-01-05 14:52:01 -0500

    simplified dataloading/collator (moved everything to preprocessing)

--- dynaprot/data/datasets.py
+++ dynaprot/data/datasets.py
@@ -3,7 +3,7 @@ import os
 from torch.utils.data import Dataset
 from openfold.data import data_transforms, feature_pipeline
 from openfold.np.protein import from_pdb_string, Protein
-import numpy as np
+# import numpy as np
 import pandas as pd
 from dynaprot.data.transforms import make_fixed_size
 from dynaprot.data.utils import dict_multimap
@@ -12,53 +12,27 @@ from dynaprot.data.utils import dict_multimap
 class DynaProtDataset(Dataset):
     
     def __init__(self, cfg):
-        self.cfg = cfg
-        self.label_dir = cfg["dynam_data_dir"]                                                                         # directory for protein dynamics labels
-        self.data_dir = cfg["struc_data_dir"]                                                                          # directory of protein structures (npz/pdb/mmcif files)
-        self.protein_list = [prot for prot in os.listdir(self.label_dir) if os.path.isdir(os.path.join(self.label_dir, prot))]  # dynamics dataset protein list
+        self.cfg = cfg                                                                     
+        self.data_dir = cfg["data_dir"]                                                                          # directory of dynaprot preprocessed proteins (tensor dicts)
+        self.protein_list = [prot for prot in os.listdir(self.data_dir) if os.path.isdir(os.path.join(self.data_dir, prot))]  # dynamics dataset protein list
 
     def __len__(self):
         return len(self.protein_list)
 
     def __getitem__(self, idx):
         protein_id = self.protein_list[idx]
-        # matching_npz =  self.protein_chains[self.protein_chains["name"] == protein_id]
-
-        try:
-            path = f"{self.data_dir}/{protein_id}.npz"
-            mmcif_feats = dict(np.load(path, allow_pickle=True))
-        except:
-            path = f"{self.data_dir}/{protein_id[1:3]}/{protein_id}.npz"
-            mmcif_feats = dict(np.load(path, allow_pickle=True))
-            
-        feats = mmcif_feats 
-        feats.pop("sequence")       
-        feats.pop("release_date")
-        feats.pop("domain_name")
+        prot_feat_dict = torch.load(os.path.join(self.data_dir,protein_id,f"{protein_id}.pt"))
+        prot_feat_dict["aatype"] = torch.eye(21)[prot_feat_dict["aatype"]]
+        prot_feat_dict["resi_pad_mask"] = torch.ones(prot_feat_dict["aatype"].shape[0])   
         
-        feats["resi_pad_mask"] = np.ones(feats["seq_length"][0])                                     # mask will be padded with zeros later on
-
-        # adding dynamics labels to escape padding
-        feats["dynamics_means"]= np.load(os.path.join(self.label_dir, f"{protein_id}/dynamics_labels/means.npy"))
-        feats["dynamics_covars"] = np.load(os.path.join(self.label_dir, f"{protein_id}/dynamics_labels/covariances.npy"))
-        
-        
-        feats = feature_pipeline.np_to_tensor_dict(feats, feats.keys())
-        
-        feats = data_transforms.atom37_to_frames(feats)             # Getting true backbone frames (num_res, 4, 4)
-        feats = data_transforms.get_backbone_frames(feats)
-
-
-        selected_feats = {k:feats[k] for k in ["aatype","residue_index","all_atom_positions","all_atom_mask", "resi_pad_mask","dynamics_means","dynamics_covars"]}
-        selected_feats["frames"] = feats["backbone_rigid_tensor"][torch.arange(feats["backbone_rigid_tensor"].shape[0]),feats['aatype'].argmax(dim=1)]     # only selecting frame for relevant residue type
-                
         shape_schema = {}
-        for k in selected_feats.keys():
-            schema = list(selected_feats[k].size())
+        for k in prot_feat_dict.keys():
+            schema = list(prot_feat_dict[k].size())
             schema[0] = "NUM_RES"   # to be infilled by padding function
             shape_schema[k] = schema
 
-        padded_selected_feats = make_fixed_size(selected_feats,shape_schema, num_residues = self.cfg["max_num_residues"])      
+        padded_selected_feats = make_fixed_size(prot_feat_dict,shape_schema, num_residues = self.cfg["max_num_residues"])      
+        # TODO: random cropping for proteins that are larger than max num res
         
         return padded_selected_feats
     
@@ -70,6 +44,35 @@ class DynaProtDataset(Dataset):
     #         num_workers=12,
     #         shuffle=False,
     #     )
+    
+    # def get_feats(self, protein_id):
+    #     try:
+    #         path = f"{self.data_dir}/{protein_id}.npz"
+    #         mmcif_feats = dict(np.load(path, allow_pickle=True))
+    #     except:
+    #         path = f"{self.data_dir}/{protein_id[1:3]}/{protein_id}.npz"
+    #         mmcif_feats = dict(np.load(path, allow_pickle=True))
+            
+    #     feats = mmcif_feats 
+    #     feats.pop("sequence")       
+    #     feats.pop("release_date")
+    #     feats.pop("domain_name")
+        
+    #     feats["resi_pad_mask"] = np.ones(feats["seq_length"][0])        # mask will be padded with zeros later on
+
+    #     feats["dynamics_means"]= np.load(os.path.join(self.label_dir, f"{protein_id}/dynamics_labels/means.npy"))
+    #     feats["dynamics_covars"] = np.load(os.path.join(self.label_dir, f"{protein_id}/dynamics_labels/covariances.npy"))
+        
+        
+    #     feats = feature_pipeline.np_to_tensor_dict(feats, feats.keys())
+        
+    #     feats = data_transforms.atom37_to_frames(feats)             # Getting true backbone frames (num_res, 4, 4)
+    #     feats = data_transforms.get_backbone_frames(feats)
+
+
+    #     selected_feats = {k:feats[k] for k in ["aatype","residue_index","all_atom_positions","all_atom_mask", "resi_pad_mask","dynamics_means","dynamics_covars"]}
+    #     selected_feats["frames"] = feats["backbone_rigid_tensor"][torch.arange(feats["backbone_rigid_tensor"].shape[0]),feats['aatype'].argmax(dim=1)]     # only selecting frame for relevant residue type
+    #     return selected_feats
         
     
 class OpenFoldBatchCollator:
@@ -83,12 +86,12 @@ if __name__ == "__main__":
     dataset = DynaProtDataset({"max_seq_len":300},"./atlas_proteins.npy","/data/cb/scratch/datasets/pdb_npz/","/data/cb/scratch/datasets/atlas_gaussians/")
 
     dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=10,
-            collate_fn=OpenFoldBatchCollator(),
-            num_workers=12,
-            shuffle=False,
-        )
+        dataset,
+        batch_size=10,
+        collate_fn=OpenFoldBatchCollator(),
+        num_workers=12,
+        shuffle=False,
+    )
 
     batch_prots = next(iter(dataloader))
 
--- dynaprot/data/utils.py
+++ dynaprot/data/utils.py
@@ -1,3 +1,31 @@
+import torch
+from openfold.np.protein import Protein
+from openfold.utils.rigid_utils import  Rigid
+from typing import Optional
+import dataclasses
+import io
+from typing import Any, Sequence, Mapping, Optional
+import re
+import string
+from openfold.np import residue_constants
+from Bio.PDB import PDBParser
+import numpy as np
+import modelcif
+import modelcif.model
+import modelcif.dumper
+import modelcif.reference
+import modelcif.protocol
+import modelcif.alignment
+import modelcif.qa_metric
+
+
+FeatureDict = Mapping[str, np.ndarray]
+ModelOutput = Mapping[str, Any]  # Is a nested dict.
+PICO_TO_ANGSTROM = 0.01
+
+PDB_CHAIN_IDS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+PDB_MAX_CHAINS = len(PDB_CHAIN_IDS)
+assert(PDB_MAX_CHAINS == 62)
 
 
 def dict_multimap(fn, dicts):
@@ -11,3 +39,117 @@ def dict_multimap(fn, dicts):
             new_dict[k] = fn(all_v)
             
     return new_dict
+
+
+
+def from_pdb_string(pdb_str: str, chain_id: Optional[str] = None) -> dict:
+    """Takes a single chain PDB string and constructs a dictionary of protein chain features.
+        Modifies the original method from openfold.np.protein
+
+    WARNING: All non-standard residue types will be converted into UNK. All
+      non-standard atoms will be ignored.
+
+    Args:
+      pdb_str: The contents of the pdb file
+      chain_id: If None, then the whole pdb file is parsed. If chain_id is specified (e.g. A), then only that chain
+        is parsed.
+
+    Returns:
+      dictionary of protein features
+    """
+    pdb_fh = io.StringIO(pdb_str)
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure("none", pdb_fh)
+    models = list(structure.get_models())
+    if len(models) != 1:
+        raise ValueError(
+            f"Only single model PDBs are supported. Found {len(models)} models."
+        )
+    model = models[0]
+
+    atom_positions = []
+    aatype = []
+    atom_mask = []
+    residue_index = []
+    chain_ids = []
+    b_factors = []
+
+    for chain in model:
+        if(chain_id is not None and chain.id != chain_id):
+            continue
+
+        for res in chain:
+            if res.id[2] != " ":
+                raise ValueError(
+                    f"PDB contains an insertion code at chain {chain.id} and residue "
+                    f"index {res.id[1]}. These are not supported."
+                )
+            res_shortname = residue_constants.restype_3to1.get(res.resname, "X")
+            restype_idx = residue_constants.restype_order.get(
+                res_shortname, residue_constants.restype_num
+            )
+            pos = np.zeros((residue_constants.atom_type_num, 3))
+            mask = np.zeros((residue_constants.atom_type_num,))
+            res_b_factors = np.zeros((residue_constants.atom_type_num,))
+            for atom in res:
+                if atom.name not in residue_constants.atom_types:
+                    continue
+                pos[residue_constants.atom_order[atom.name]] = atom.coord
+                mask[residue_constants.atom_order[atom.name]] = 1.0
+                res_b_factors[
+                    residue_constants.atom_order[atom.name]
+                ] = atom.bfactor
+            if np.sum(mask) < 0.5:
+                # If no known atom positions are reported for the residue then skip it.
+                continue
+
+            aatype.append(restype_idx)
+            atom_positions.append(pos)
+            atom_mask.append(mask)
+            residue_index.append(res.id[1])
+            chain_ids.append(chain.id)
+            b_factors.append(res_b_factors)
+
+    # parents = None
+    # parents_chain_index = None
+    # if("PARENT" in pdb_str):
+    #     parents = []
+    #     parents_chain_index = []
+    #     chain_id = 0
+    #     for l in pdb_str.split("\n"):
+    #         if("PARENT" in l):
+    #             if(not "N/A" in l):
+    #                 parent_names = l.split()[1:]
+    #                 parents.extend(parent_names)
+    #                 parents_chain_index.extend([
+    #                     chain_id for _ in parent_names
+    #                 ])
+    #             chain_id += 1
+
+    # unique_chain_ids = np.unique(chain_ids)
+    # chain_id_mapping = {cid: n for n, cid in enumerate(string.ascii_uppercase)}
+    # chain_index = np.array([chain_id_mapping[cid] for cid in chain_ids])
+
+    return dict(
+        aatype= np.array(aatype),
+        all_atom_positions= np.array(atom_positions),
+        all_atom_mask=np.array(atom_mask),
+        residue_index=np.array(residue_index),
+        # chain_index=chain_index,
+        b_factors=np.array(b_factors),
+        # parents=parents,
+        # parents_chain_index=parents_chain_index,
+    )
+    
+    
+def align_one_protein(feats):
+    """Aligns the covariance matrices of a single protein to its local frames."""
+    prot_frames, prot_covars = feats["frames"].double(), feats["dynamics_covars"].double()
+    
+    # print(prot_frames.shape[0], prot_covars.shape[0])
+    assert prot_frames.shape[0] == prot_covars.shape[0]
+    rotations = Rigid.from_tensor_4x4(prot_frames).get_rots().get_rot_mats().double()  # Extract rotation matrices
+    aligned_covars = torch.einsum("nij,njk,nlk->nil", rotations, prot_covars, rotations)
+
+    return aligned_covars
+   
