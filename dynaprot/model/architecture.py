4247a16116375b6283788023c0807be597dabd6d
MihirBafna <mihir.bafna@gmail.com>
2025-02-24 14:12:42 -0500

    working version

--- dynaprot/model/architecture.py
+++ dynaprot/model/architecture.py
@@ -62,7 +62,7 @@ class DynaProt(LightningModule):
             residue_features = ipa_block(residue_features, pairwise_embeddings, frames, mask)
             residue_features = self.dropout(residue_features)
 
-        # residue_features = self.layer_norm(residue_features)
+        residue_features = self.layer_norm(residue_features)
 
         preds = dict(
             # means = self.pred_mean(residue_features),      # Shape: (batch_size, num_residues, 3)
