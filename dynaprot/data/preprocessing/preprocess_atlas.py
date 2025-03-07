5c28ba509df6b150cb48d9b6a3460f6c93fbdf47
MihirBafna <mihir.bafna@gmail.com>
2025-03-07 12:58:18 -0500

    removed print

--- dynaprot/data/preprocessing/preprocess_atlas.py
+++ dynaprot/data/preprocessing/preprocess_atlas.py
@@ -63,17 +63,19 @@ def save_separate_frames(prot, superposed_traj, frame_stride=20):
         with tempfile.NamedTemporaryFile(suffix=".pdb", delete=True) as tmp_pdb:
             traj[i].save_pdb(tmp_pdb.name)  # Save PDB to temp file
             pdb_string = tmp_pdb.read().decode()
-            print(pdb_string)
+            # print(pdb_string)
         feats = from_pdb_string(pdb_string)
         feats = feature_pipeline.np_to_tensor_dict(feats, feats.keys())
         feats = data_transforms.atom37_to_frames(feats)
         feats = data_transforms.get_backbone_frames(feats)
         torch.save({"frames":feats["backbone_rigid_tensor"]}, frame_path)
         
+    return prot
+        
 
 def process_one_trajectory_atlas(prot):   # just taking R1 traj and topology file as pdb. Is this valid??
     name,chain = prot.split("_")
-    for i in range(1):
+    for i in range(3):
         traj_path = os.path.join(inpath, prot, prot+f"_prod_R{i+1}_fit.xtc")
         pdb_path = os.path.join(inpath,prot, prot+".pdb")
         
@@ -84,25 +86,25 @@ def process_one_trajectory_atlas(prot):   # just taking R1 traj and topology fil
         ref = md.load(pdb_path)     
         traj.superpose(ref)         # superpose to our mmcifs
 
-        save_separate_frames(prot, traj, 100)
+        return save_separate_frames(prot, traj, 100)
         
-        # # generate feats and process them into dicts
-        # feats = from_pdb_string(open(pdb_path, 'r').read())
-        # feats = feature_pipeline.np_to_tensor_dict(feats, feats.keys()) # converting to tensor dict
-        # feats = data_transforms.atom37_to_frames(feats)                 # Getting true backbone frames (num_res, 4, 4)
-        # feats = data_transforms.get_backbone_frames(feats)
-        # selected_feats = {k:feats[k] for k in ["aatype","residue_index","all_atom_positions","all_atom_mask"]}
-        # selected_feats["frames"] = feats["backbone_rigid_tensor"] 
+        # generate feats and process them into dicts
+        feats = from_pdb_string(open(pdb_path, 'r').read())
+        feats = feature_pipeline.np_to_tensor_dict(feats, feats.keys()) # converting to tensor dict
+        feats = data_transforms.atom37_to_frames(feats)                 # Getting true backbone frames (num_res, 4, 4)
+        feats = data_transforms.get_backbone_frames(feats)
+        selected_feats = {k:feats[k] for k in ["aatype","residue_index","all_atom_positions","all_atom_mask"]}
+        selected_feats["frames"] = feats["backbone_rigid_tensor"] 
 
-        # # compute all dynamics here                 
-        # selected_feats["dynamics_means"], selected_feats["dynamics_covars"],selected_feats["dynamics_fullcovar"] = compute_gaussians_per_residue(traj, args.calpha)
-        # # selected_feats["dynamics_fullcovar"] = compute_full_covariance(traj, args.calpha)
+        # compute all dynamics here                 
+        selected_feats["dynamics_means"], selected_feats["dynamics_covars"],selected_feats["dynamics_fullcovar"] = compute_gaussians_per_residue(traj, args.calpha)
+        # selected_feats["dynamics_fullcovar"] = compute_full_covariance(traj, args.calpha)
         
-        # selected_feats["dynamics_covars"], selected_feats["dynamics_fullcovar"] = map_one_protein_local_frame(selected_feats)
+        selected_feats["dynamics_covars"], selected_feats["dynamics_fullcovar"] = map_one_protein_local_frame(selected_feats)
 
-        # selected_feats["dynamics_correlations"] = compute_residue_correlations(selected_feats["dynamics_fullcovar"])
+        selected_feats["dynamics_correlations"] = compute_residue_correlations(selected_feats["dynamics_fullcovar"])
         
-        # torch.save(selected_feats,os.path.join(outpath,prot,f"{prot}_rep{i+1}.pt"))
+        torch.save(selected_feats,os.path.join(outpath,prot,f"{prot}_rep{i+1}.pt"))
         
     return prot
 
@@ -214,7 +216,7 @@ def compute_residue_correlations(full_cov, mode="trace"):  # using trace or fro 
             
 
 
-process_one_trajectory_atlas("1bq8_A")
+# process_one_trajectory_atlas("1bq8_A")
 
-# preprocess_atlas()
+preprocess_atlas()
 
